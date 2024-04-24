a4d386435ace756c129b31797343a8c97c84007f
arnabw <arnabsahawrk@gmail.com>
2024-04-24 14:16:06 +0600

    initial commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import { Helmet } from "react-helmet-async";
+
+const HomePage = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Home</title>
+      </Helmet>
+      <section>
+        <div className="container mx-auto px-4"></div>
+      </section>
+    </>
+  );
+};
+
+export default HomePage;
